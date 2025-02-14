8754b92757b98f067dccdabc92432fd5c84e72f5
Manjur-WD <114661279+Manjur-WD@users.noreply.github.com>
2025-02-14 12:04:20 +0530

    updated

--- src/components/common/AdminHead.jsx
+++ src/components/common/AdminHead.jsx
@@ -50,11 +50,11 @@ const AdminHead = ({ breadcrumb_name }) => {
             <div className="user-btn px-5">
                 <details className="relative shadow-lg rounded-xl">
                     <summary className="list-none">
-                        <span className="inline-block p-2 px-5"><FaUserLarge className="inline me-1 bg-whitesmoke text-2xl p-1 rounded-full " />{user.role}</span>
+                        <span className="inline-block p-2 px-5"><FaUserLarge className="inline me-1 bg-whitesmoke text-2xl p-1 rounded-full " />{user.name}{" "}({user.role})</span>
                     </summary>
                     <ul className="absolute border z-50 bg-white top-[110%] rounded-2xl text-nowrap right-0 shadow-lg overflow-hidden p-2">
-                        <li className="cursor-pointer hover:text-white hover:bg-black px-3 py-2 rounded-xl border mb-1" onClick={()=>navigate("/change-password")} >< TbPasswordFingerprint className="inline me-1" />Change Password</li>
-                        <li className="cursor-pointer hover:text-white hover:bg-black px-3 py-2 rounded-xl border" onClick={() => dispatch(logout())}><CgLogOut className="inline me-1"/>Logout</li>
+                        <li className="cursor-pointer hover:text-white hover:bg-black px-3 py-2 rounded-xl border mb-1" onClick={() => navigate("/change-password")} >< TbPasswordFingerprint className="inline me-1" />Change Password</li>
+                        <li className="cursor-pointer hover:text-white hover:bg-black px-3 py-2 rounded-xl border" onClick={() => dispatch(logout())}><CgLogOut className="inline me-1" />Logout</li>
                     </ul>
                 </details>
 
