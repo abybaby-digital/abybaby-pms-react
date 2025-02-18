93fc01091cf7a3a94480f386e8313abdbcaf7448
Manjur-WD <114661279+Manjur-WD@users.noreply.github.com>
2025-02-18 10:14:29 +0530

    updated

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4dvntd3qvn8"
+    "revision": "0.nktet3s89oo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
